26c07ba60d03756a8461418a94fd7033e518e093
Loopy <91286502+Wizzable@users.noreply.github.com>
2022-03-11 13:26:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import ast, inspect, re, discord
+from discord.ext import commands
+
+def source(o):
+    s = inspect.getsource(o).split("\n")
+    indent = len(s[0]) - len(s[0].lstrip())
+    return "\n".join(i[indent:] for i in s)
+
+
+source_ = source(discord.gateway.DiscordWebSocket.identify)
+patched = re.sub(
+    r'([\'"]\$browser[\'"]:\s?[\'"]).+([\'"])', 
+    r"\1Discord Android\2",  
+    source_
+)
+
+loc = {}
+exec(compile(ast.parse(patched), "<string>", "exec"), discord.gateway.__dict__, loc)
+
+discord.gateway.DiscordWebSocket.identify = loc["identify"]
+
+
+client = commands.Bot(command_prefix = ".")
+
+@client.event
+async def on_ready():
+  await client.change_presence(activity=discord.Game(name="a game"))
+
+client.run("")
